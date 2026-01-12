8543dbe3cd3a1a8021247ce0a586cd1628dc21f5
Teggy404 <oscarsal0223@gmail.com>
2026-01-11 19:52:55 -0800

    (feat) setup up docker compose for database and create initial User schema

---
+++ backend/Dtos/AuthRequest.cs
@@ -0,0 +1,8 @@
+﻿namespace backend.Dtos;
+
+public class AuthRequest
+{
+    public record RegisterRequest(String Email, String Password);
+    public record LoginRequest(String Email, String Password);
+    public record AuthResponse(String Token);
+}
